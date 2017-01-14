3541f3972bd28526409bc0c11559be87f856ff74
stgleb <glebstepanov1992@gmail.com>
2017-01-14 14:34:51 +0200

    Add limit conf and ability to change limit configuration in runtime

--- limit.go
+++ limit.go
@@ -7,13 +7,22 @@ import (
 
 type Token struct{}
 
+type LimitConf struct {
+	Name      string  `json:"name"`
+	Interval  int64   `json:"interval"`
+	Count     int64   `json:"count"`
+	Precision float64 `json:"precision"`
+}
+
 type Limit struct {
-	Name      string        `json:"name"`
-	Interval  int64         `json:"interval"`
-	Precision float64       `json:"precision"`
-	Count     int64         `json:"count"`
-	Output    chan Token    `json:"-"`
-	ShutDown  chan struct{} `json:"-"`
+	Name      string         `json:"name"`
+	Interval  int64          `json:"interval"`
+	Precision float64        `json:"precision"`
+	Count     int64          `json:"count"`
+	Output    chan Token     `json:"-"`
+	ShutDown  chan struct{}  `json:"-"`
+	Update    chan LimitConf `json:"-"`
+	GetConf   chan LimitConf `json:"-"`
 }
 
 // Create new limit, interval is set in milliseconds.
@@ -34,6 +43,12 @@ func NewLimit(name string, interval, count int64, precision float64) *Limit {
 func (limit *Limit) Run() {
 	updateInterval := time.Duration(limit.Interval / limit.Count)
 	ticker := time.NewTicker(time.Duration(updateInterval) * time.Millisecond)
+	currentConf := LimitConf{
+		Name:      limit.Name,
+		Count:     limit.Count,
+		Interval:  limit.Interval,
+		Precision: limit.Precision,
+	}
 
 	for {
 		select {
@@ -43,11 +58,23 @@ func (limit *Limit) Run() {
 		case <-limit.ShutDown:
 			Info.Printf("Channel %s shut down", limit.Name)
 			return
+		case currentConf = <-limit.Update:
+			// TODO(stgleb) consider check if values were changed.
+			limit.Count = currentConf.Count
+			limit.Interval = currentConf.Interval
+			limit.Precision = currentConf.Precision
+
+			if len(currentConf.Name) > 0 {
+				limit.Name = currentConf.Name
+			}
+		case limit <- currentConf:
+			// Try to always send current configuration to external
+			// consumer.
 		}
 	}
 }
 
-func (limit *Limit) String() string {
+func (limit Limit) String() string {
 	return fmt.Sprintf("<Name: %s, Interval: %d, Count: %d, Precision: %f>",
 		limit.Name, limit.Interval, limit.Count, limit.Precision)
 }
